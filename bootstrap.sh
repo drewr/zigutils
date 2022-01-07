6f6470caea866e5ffd477350bcd0aaa331559470
Drew Raines <drew@raines.me>
2022-01-07 13:27:08 -0600

    Handle both submodule states

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,26 @@
 #!/bin/sh
 
-for p in libcurl libssh2 zlib mbedtls; do
-  git submodule add https://github.com/mattnite/zig-$p lib/zig-$p
-  ( cd lib/zig-$p && git submodule update --init )
-done
+bootstrapped=
+
+if git submodule status | fgrep -q libcurl; then
+  bootstrapped=1
+fi
+
+boot_submodules () {
+  for p in libcurl libssh2 zlib mbedtls; do
+    git submodule add https://github.com/mattnite/zig-$p lib/zig-$p
+    ( cd lib/zig-$p && git submodule update --init )
+  done
+}
+
+update_submodules () {
+  git submodule update --init --recursive
+}
+
+if [ -z $bootstrapped ]; then
+  echo retrieving libraries
+  boot_submodules
+else
+  echo updating libraries
+  update_submodules
+fi
