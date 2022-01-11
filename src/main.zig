0163b14454d5b335019dedbd46610712c66b57d2
Drew Raines <drew@raines.me>
2022-01-11 16:11:19 -0600

    Add (non-working) attempt at adapting the C string

--- src/main.zig
+++ src/main.zig
@@ -5,12 +5,17 @@ const curl = @cImport({
 });
 
 pub fn main() anyerror!void {
+    var host: []u8 = null;
+
     const url = "http://example.com/path/index.html";
     const h = curl.curl_url();
-    std.log.debug("{any}", .{h});
+    std.log.debug("curl handle: {any}", .{h});
 
-    const uc = curl.curl_url_set(h, curl.CURLUPART_URL, url, 0);
-    std.log.debug("{any}", .{uc});
+    var uc = curl.curl_url_set(h, curl.CURLUPART_URL, url, 0);
+    std.log.debug("uc: {any}", .{uc});
+
+    uc = curl.curl_url_get(h, curl.CURLUPART_HOST, host, 1);
+    std.log.debug("host ({any}): {s}", .{ uc, std.mem.sliceTo(host, 0) });
 }
 
 test "basic test" {
