ca9ca880fe6f03da0957cc91fef90e3b01dfab74
Drew Raines <drew@raines.me>
2022-01-16 16:06:07 -0600

    WIP factor out URL parsing

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,5 @@
 const std = @import("std");
-
+const Allocator = std.mem.Allocator;
 const curl = @cImport({
     @cInclude("curl/curl.h");
 });
@@ -13,17 +13,7 @@ pub fn main() anyerror!void {
 
     var arg_iter = std.process.args();
     while (try arg_iter.next(alloc)) |url| {
-        var host: ?[*:0]u8 = null;
-        var path: ?[*:0]u8 = null;
-
-        var uc = curl.curl_url_set(h, curl.CURLUPART_URL, url, 0);
-        if (uc == 0) {
-            _ = curl.curl_url_get(h, curl.CURLUPART_HOST, &host, 1);
-            _ = curl.curl_url_get(h, curl.CURLUPART_PATH, &path, 1);
-            const arr = split_path(std.mem.span(path.?));
-            std.log.info("{s} / {s}", .{ arr[0], arr[1] });
-        }
-        alloc.free(url);
+        parseUrl(alloc, h, url);
     }
 }
 
@@ -31,11 +21,26 @@ test "basic test" {
     try std.testing.expectEqual(10, 3 + 7);
 }
 
-fn split_path(path: []const u8) [2][]const u8 {
+fn splitPath(path: []const u8) [2][]const u8 {
     var it = std.mem.split(u8, path, "/");
     _ = it.next().?; // emptiness before the slash
     const user = it.next().?;
     const repo = it.next().?;
     // sourcehut uses tildes like unix homedirs, gotta strip that
     return .{ if (user[0] == '~') user[1..] else user, repo };
+}
+
+// ERROR: ./src/main.zig:16:25: error: expected type '*.cimport:3:14.struct_Curl_URL', found '?*.cimport:3:14.struct_Curl_URL'
+fn parseUrl(alloc: Allocator, h: *curl.struct_Curl_URL, url: []const u8) void {
+    var host: ?[*:0]u8 = null;
+    var path: ?[*:0]u8 = null;
+
+    var uc = curl.curl_url_set(h, curl.CURLUPART_URL, url, 0);
+    if (uc == 0) {
+        _ = curl.curl_url_get(h, curl.CURLUPART_HOST, &host, 1);
+        _ = curl.curl_url_get(h, curl.CURLUPART_PATH, &path, 1);
+        const arr = splitPath(std.mem.span(path.?));
+        std.log.info("{s} / {s}", .{ arr[0], arr[1] });
+    }
+    alloc.free(url);
 }
