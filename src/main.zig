769f711bd627aeddab60192ab0fb30aa0f6a8544
Drew Raines <drew@raines.me>
2022-01-16 17:40:29 -0600

    Introduce Repo type

--- src/main.zig
+++ src/main.zig
@@ -4,6 +4,11 @@ const curl = @cImport({
     @cInclude("curl/curl.h");
 });
 
+const Repo = struct {
+    user: []const u8 = undefined,
+    name: []const u8 = undefined,
+};
+
 pub fn main() anyerror!void {
     var alloc_buf: [1024]u8 = undefined;
     var fba = std.heap.FixedBufferAllocator.init(&alloc_buf);
@@ -13,7 +18,14 @@ pub fn main() anyerror!void {
 
     var arg_iter = std.process.args();
     while (try arg_iter.next(alloc)) |url| {
-        parseUrl(alloc, h, url);
+        const repo = parseUrl(alloc, h, url);
+        if (repo == null) {
+            //not sure why this prints \252\252\252...
+            //std.log.warn("can't parse: {s}", .{url});
+            continue;
+        } else {
+            std.log.debug("{s} {s}", .{ repo.?.user, repo.?.name });
+        }
     }
 }
 
@@ -21,27 +33,31 @@ test "basic test" {
     try std.testing.expectEqual(10, 3 + 7);
 }
 
-fn splitPath(path: []const u8) [2][]const u8 {
+fn makeRepo(path: []const u8) Repo {
     var it = std.mem.split(u8, path, "/");
     _ = it.next().?; // emptiness before the slash
     const user = it.next().?;
     const repo = it.next().?;
     // sourcehut uses tildes like unix homedirs, gotta strip that
-    return .{ if (user[0] == '~') user[1..] else user, repo };
+    return Repo{ .user = if (user[0] == '~') user[1..] else user, .name = repo };
 }
 
-fn parseUrl(alloc: Allocator, h: ?*curl.struct_Curl_URL, url: [:0]const u8) void {
+fn parseUrl(alloc: Allocator, h: ?*curl.struct_Curl_URL, url: [:0]const u8) ?*Repo {
     var host: ?[*:0]u8 = null;
     var path: ?[*:0]u8 = null;
+    var repo: Repo = Repo{};
 
     var uc = curl.curl_url_set(h, curl.CURLUPART_URL, url, 0);
+    defer alloc.free(url);
+
     if (uc == 0) {
         _ = curl.curl_url_get(h, curl.CURLUPART_HOST, &host, 1);
         _ = curl.curl_url_get(h, curl.CURLUPART_PATH, &path, 1);
-        const arr = splitPath(
+        repo = makeRepo(
         // the span() converts the slice to a []const u8
         std.mem.span(path.?));
-        std.log.info("{s} / {s}", .{ arr[0], arr[1] });
+        return &repo;
+    } else {
+        return null;
     }
-    alloc.free(url);
 }
