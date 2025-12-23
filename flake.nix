326b4c79d9cdb77dcd005ee08ee876c02b15af85
Drew Raines <drew@raines.me>
2025-12-23 11:40:31 -0600

    Add nix flake

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  description = "Zig utilities including nix-zsh-env";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages = {
+          nix-zsh-env = pkgs.stdenv.mkDerivation {
+            pname = "nix-zsh-env";
+            version = "0.1.0";
+            src = ./.;
+
+            nativeBuildInputs = [ pkgs.zig ];
+
+            buildPhase = ''
+              zig build-exe src/nix-zsh-env.zig -O ReleaseSafe
+            '';
+
+            installPhase = ''
+              mkdir -p $out/bin
+              cp nix-zsh-env $out/bin/
+            '';
+
+            meta = {
+              description = "Display nix-shell packages for zsh prompt";
+              license = pkgs.lib.licenses.mit;
+              platforms = pkgs.lib.platforms.all;
+            };
+          };
+
+          default = self.packages.${system}.nix-zsh-env;
+        };
+
+        apps = {
+          nix-zsh-env = {
+            type = "app";
+            program = "${self.packages.${system}.nix-zsh-env}/bin/nix-zsh-env";
+          };
+          default = self.apps.${system}.nix-zsh-env;
+        };
+      }
+    );
+}
